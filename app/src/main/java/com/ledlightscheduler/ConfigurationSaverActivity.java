0d57b9e2cb789072d31bc5dd9ebc2e5b1784f483
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-08-10 11:31:49 -0700

    FEATURE: FileSaverAndLoader can now handle saving multiple files
    This commit allows the FileSaverAndLoader to save and retrieve multiple different sets of LEDStrips. This allows a user to save multiple different configurations. By default, the configuration is saved to Default.txt.
    
    Resolves: The lack of being able to store multiple configurations.

--- app/src/main/java/com/ledlightscheduler/ConfigurationSaverActivity.java
+++ app/src/main/java/com/ledlightscheduler/ConfigurationSaverActivity.java
@@ -2,16 +2,47 @@ package com.ledlightscheduler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfigurationSaverActivity extends Activity {
+
+    private Button saveConfigButton;
+    private Button goBackToMainActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration_selection);
 
-        
+        setupUIElements();
+        setupButtons();
 
+    }
+
+    public void setupUIElements(){
+        saveConfigButton = findViewById(R.id.SaveThisConfigurationButton);
+        goBackToMainActivityButton = findViewById(R.id.GoBackToMainActivityButton);
+    }
+
+    public void setupButtons(){
+        saveConfigButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+
+        goBackToMainActivityButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
     }
 
 }
--- app/src/main/java/com/ledlightscheduler/FileSaverAndLoader.java
+++ app/src/main/java/com/ledlightscheduler/FileSaverAndLoader.java
@@ -1,50 +1,78 @@
 package com.ledlightscheduler;
 
 import android.app.Activity;
-import android.content.Context;
+import android.util.Log;
 
 import com.ledlightscheduler.arduinopackets.packets.LEDStripsInformationPacket;
 import com.ledlightscheduler.ledstriputilities.ledstrips.SingleColorLEDStrip;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileSaverAndLoader {
 
-    private static final String saveFileDirectory = "LEDStripSequences.txt";
+    private static final String saveFileDirectory = "LEDStripSequences";
 
-    public static void saveLEDStrips(ArrayList<SingleColorLEDStrip> ledStrips, Activity activity){
+    public static void saveLEDStrips(ArrayList<SingleColorLEDStrip> ledStrips, Activity activity, String name){
+        checkIfSaveDirExisted(activity);
         try {
-            FileOutputStream outputStream = activity.openFileOutput(saveFileDirectory, Context.MODE_PRIVATE);
-            outputStream.write(new LEDStripsInformationPacket(ledStrips).serialize().getBytes());
-            outputStream.close();
+            String pathName = activity.getFilesDir().getAbsolutePath() + "/" + saveFileDirectory + "/" + name + ".txt";
+            File newFile = new File(pathName);
+            if (!newFile.createNewFile()) {
+                Log.w("[LEDLightStripScheduler]", "Couldn't create file " + name + ".txt.");
+            }
+            FileWriter writer = new FileWriter(pathName);
+            writer.write(new LEDStripsInformationPacket(ledStrips).serialize());
+            writer.close();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public static ArrayList<SingleColorLEDStrip> getLEDStrips(Activity activity){
+    public static File[] getLEDStripFiles(Activity activity){
         try {
-            FileInputStream inputStream = activity.openFileInput(saveFileDirectory);
-            InputStreamReader reader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            StringBuilder output = new StringBuilder();
-            String currText;
-            while ((currText = bufferedReader.readLine()) != null) {
-                output.append(currText).append("\n");
-            }
-            LEDStripsInformationPacket packet = new LEDStripsInformationPacket().deserialize(output.toString());
-            bufferedReader.close();
-            reader.close();
-            inputStream.close();
-            return packet.getLEDStrips();
+            File directory = new File(activity.getFilesDir() + "/" + saveFileDirectory);
+            return directory.listFiles((File pathname) -> pathname.getName().endsWith(".txt"));
         } catch (Exception e){
             e.printStackTrace();
         }
+        return new File[0];
+    }
+
+    public static ArrayList<SingleColorLEDStrip> getLEDStrips (Activity activity, String name){
+        if(checkIfSaveDirExisted(activity)){
+            File possibleFile = new File(activity.getFilesDir().getAbsolutePath() + "/" + saveFileDirectory + "/" + name + ".txt");
+            if(possibleFile.exists()){
+                try {
+                    Scanner scanner = new Scanner(possibleFile);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while(scanner.hasNextLine()) {
+                        stringBuilder.append(scanner.nextLine());
+                    }
+                    return new LEDStripsInformationPacket().deserialize(stringBuilder.toString()).getLEDStrips();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
         return new ArrayList<>();
+    }
+
+    private static boolean checkIfSaveDirExisted(Activity activity) {
+        Path path = Paths.get(activity.getFilesDir().getAbsolutePath() + "/" + saveFileDirectory);
+        if (Files.notExists(path)){
+            File directory = path.toFile();
+            if (!directory.mkdir()) {
+                Log.w("[LEDLightStripScheduler]","Unable to create the save directory!");
+            }
+            return false;
+        }
+        return true;
     }
 
 }
