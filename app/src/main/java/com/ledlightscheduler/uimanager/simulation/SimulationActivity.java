12574a3a7096b684302f31377f4c6849a5566fc6
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-08-17 17:58:41 -0700

    FEATURE: Designed Simulation Page
    This commit adds the Simulation Page, which consists of the text of Simulation, a button to go back to the MainActivity, and a RecyclerView which will contain cards that will simulate.
    
    Resolves: Lack of view to represent the simulation page.

--- app/src/main/java/com/ledlightscheduler/uimanager/simulation/SimulationActivity.java
+++ app/src/main/java/com/ledlightscheduler/uimanager/simulation/SimulationActivity.java
@@ -1,9 +1,16 @@
 package com.ledlightscheduler.uimanager.simulation;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.ledlightscheduler.R;
 
 public class SimulationActivity extends Activity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulation);
+    }
 
 }
